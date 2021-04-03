3e2035340de07fa8b9a0f8345ec1fcf6a81ab156
Mahdi Parastesh <fulcrum1378@gmail.com>
2021-04-04 01:43:47 +0430

    2021.04.04 - 1

--- app/src/main/java/org/ifaco/mergen/Panel.kt
+++ app/src/main/java/org/ifaco/mergen/Panel.kt
@@ -1,22 +1,22 @@
 package org.ifaco.mergen
 
+import android.Manifest
 import android.media.MediaPlayer
 import android.net.Uri
 import android.os.*
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import org.ifaco.mergen.Fun.Companion.c
 import org.ifaco.mergen.Fun.Companion.permResult
 import org.ifaco.mergen.databinding.PanelBinding
 import org.ifaco.mergen.pro.Communicator
 import org.ifaco.mergen.pro.Writer
-import org.ifaco.mergen.rtc.Client
 
 class Panel : AppCompatActivity() {
     lateinit var b: PanelBinding
     val model: Model by viewModels()
-    lateinit var client: Client
     lateinit var pro: Writer
     lateinit var com: Communicator
 
@@ -38,7 +38,8 @@ class Panel : AppCompatActivity() {
         handler = object : Handler(Looper.getMainLooper()) {
             override fun handleMessage(msg: Message) {
                 when (msg.what) {
-                    Action.RECORD.ordinal -> client.record()
+                    Action.RECORD.ordinal -> {
+                    }
                     Action.WRITE.ordinal -> msg.obj?.let { b.say.setText("$it") }
                     Action.PRONOUNCE.ordinal -> {
                         mp = MediaPlayer.create(this@Panel, msg.obj as Uri)
@@ -55,24 +56,10 @@ class Panel : AppCompatActivity() {
         pro = Writer(this, model, b.body, b.response, b.resSV, b.say, b.clear)
         com = Communicator(this, b.say, model)
 
-        // RTC
-        client = Client(this, b.renderer)
-        b.body.setOnLongClickListener {
-            client.callMe()
-            true
-        }
-    }
-
-    override fun onResume() {
-        super.onResume()
-    }
-
-    override fun onPause() {
-        super.onPause()
+        permissions()
     }
 
     override fun onDestroy() {
-        client.destroy()
         try {
             mp?.release()
         } catch (ignored: Exception) {
@@ -93,9 +80,13 @@ class Panel : AppCompatActivity() {
     }
 
 
+    fun permissions() {
+        if (Fun.permGranted(Manifest.permission.RECORD_AUDIO) && Fun.permGranted(Manifest.permission.CAMERA))
+            handler?.obtainMessage(Action.RECORD.ordinal)?.sendToTarget()
+        else ActivityCompat.requestPermissions(
+            this, arrayOf(Manifest.permission.RECORD_AUDIO, Manifest.permission.CAMERA), reqRecord
+        )
+    }
+
     enum class Action { RECORD, WRITE, PRONOUNCE, SOCKET }
 }
-
-// adb tcpip 5555
-// adb connect 192.168.1.4:5555
-// adb kill-server
